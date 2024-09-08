cfa536b4eee8b10bf80cfd59600b8ba8427abdef
ShobujDas <shobujd6.gmail.com>
2024-09-08 12:30:55 +0600

    solved Messager and Done all error solved

---
+++ server/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const connectDB = require('./config/connectDB');
+const router = require('./routers/index');
+const cookieParser = require('cookie-parser');
+const {app,server} = require('./socket/index')
+
+
+// const app = express();
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server runngin at port " + PORT,
+    success:true
+  })
+})
+
+
+
+// api endpints
+app.use("/api", router);
+
+
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log("server running at " + PORT);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
