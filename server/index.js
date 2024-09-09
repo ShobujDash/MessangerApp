fc9a6c2896aa7706f85ca39792815d3a36e3ecc8
ShobujDash <158812287+ShobujDash@users.noreply.github.com>
2024-09-09 21:41:17 +0600

    Update cors url index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const {app,server} = require('./socket/index')
 // const app = express();
 app.use(
   cors({
-    origin: "https://shobujmessanger.netlify.app",
+    origin: "https://shobuj-messanger.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
